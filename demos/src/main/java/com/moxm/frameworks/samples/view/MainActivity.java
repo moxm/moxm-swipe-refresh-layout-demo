9ac9dbbe6f2af655b4daedfef9bb73419a1b26fb
马祥力 <xiangli.ma@newtouch.cn>
2015-07-07 15:57:29 +0800

    Initial commit

---
+++ demos/src/main/java/com/moxm/frameworks/samples/view/MainActivity.java
@@ -0,0 +1,60 @@
+/*
+ * Created by Storm Zhang, Mar 31, 2014.
+ */
+
+package com.moxm.frameworks.samples.view;
+
+import android.os.Bundle;
+import android.os.Handler;
+import android.support.v4.widget.SwipeRefreshLayout;
+import android.support.v7.app.ActionBarActivity;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+import com.moxm.frameworks.samples.R;
+
+import java.util.ArrayList;
+
+/**
+ * @author Richard Ma
+ */
+public class MainActivity extends ActionBarActivity implements SwipeRefreshLayout.OnRefreshListener {
+
+	private SwipeRefreshLayout mSwipeLayout;
+	private ListView mListView;
+	private ArrayList<String> list = new ArrayList<String>();
+
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.item_main);
+
+		mListView = (ListView) findViewById(R.id.listview);
+		mListView.setAdapter(new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1,
+				getData()));
+
+		mSwipeLayout = (SwipeRefreshLayout) findViewById(R.id.swipe_container);
+		mSwipeLayout.setOnRefreshListener(this);
+		mSwipeLayout.setColorSchemeResources(android.R.color.holo_blue_bright,
+				android.R.color.holo_green_light, android.R.color.holo_orange_light,
+				android.R.color.holo_red_light);
+	}
+
+	private ArrayList<String> getData() {
+		list.add("data 1");
+		list.add("data 2");
+		list.add("data 3");
+		list.add("data 4");
+		list.add("data 5");
+		list.add("data 6");
+		return list;
+	}
+
+	public void onRefresh() {
+		new Handler().postDelayed(new Runnable() {
+			@Override
+			public void run() {
+				mSwipeLayout.setRefreshing(false);
+			}
+		}, 5000);
+	}
+}
